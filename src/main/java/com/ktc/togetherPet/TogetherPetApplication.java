b381c33420f79b04029a7003584351b6920347e0
pinkbeenyeti <rbgusdn12@gmail.com>
2024-09-19 21:13:21 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/main/java/com/ktc/togetherPet/TogetherPetApplication.java
@@ -0,0 +1,13 @@
+package com.ktc.togetherPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherPetApplication.class, args);
+	}
+
+}
