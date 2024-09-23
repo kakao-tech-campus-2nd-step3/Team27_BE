f11f6d5105d21e0ff173b1802ca3bf14e0e3c4c2
Lee Hyungi <63250720+geist9110@users.noreply.github.com>
2024-09-23 18:16:56 +0900

    [코드리뷰 반영] 엔티티 변수명에서 테이블명 제거 및 연관관계 추가
    * [코드리뷰 반영] 엔티티 변수명에서 테이블명 제거 및 연관관계 추가 (#23)
    
    * refactor: 엔티티 변수명에서 테이블명 제거
    
    멘토님 코드리뷰 반영사항으로 엔티티에서 변수명을 제거
    
    Related to: #7
    
    * feat: mysql 데이터베이스 연결 및 gitignore 추가
    
    mysql 데이터베이스와 엔티티 연결
    gitignore에 .env.properties 추가
    
    Related to: #7
    
    * feat: Region과 User 관계 추가
    
    User 엔티티에 Region 엔티티를 ManyToOne으로 받도록 구현
    
    Realted to: #7
    
    * feat: User와 Pet 관계 추가
    
    User 엔티티에 Pet 엔티티를 ManyToOne으로 받도록 구현
    
    Related to: #7
    
    * feat: Pet과 Breed 관계 추가
    
    Pet 엔티티에서 Breed 엔티티로 ManyToOne 연관 관계 추가
    
    Related to: #7
    
    * feat: Missing과 Pet 관계 추가
    
    Missing 엔티티에서 Pet 엔티티로 ManyToOne 연관 관계 추가
    
    Related to: #7
    
    * feat: Report 연관관계 추가
    
    Report엔티티에서 User엔티티로 OneToOne 연관관계 추가
    Report엔티티에서 Missing엔티티로 ManyToOne 연관관계 추가
    
    Related to: #7
    
    * feat: ImageRelation 엔티티 연관관계 추가
    
    ImageRelation엔티티에서 Image엔티티로 ManyToOne관계 추가
    ImageEntityType Enum클래스를 추가하여 Mysql에서 자동으로 Enum 테이블 생성하도록 변경
    
    Realated to: #7
    
    ---------
    
    Co-authored-by: pinkbeenyeti <rbgusdn12@gmail.com>
    Co-authored-by: Hoseong Jeong <42439493+yoong-saks@users.noreply.github.com>

--- src/main/java/com/ktc/togetherPet/model/entity/User.java
+++ src/main/java/com/ktc/togetherPet/model/entity/User.java
@@ -1,24 +1,34 @@
 package com.ktc.togetherPet.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long id;
 
-    @Column(name = "region_id", nullable = false)
-    private Long regionId;
+    @ManyToOne(targetEntity = Region.class)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region;
 
-    @Column(name = "pet_id", nullable = false)
-    private Long petId;
+    @ManyToOne(targetEntity = Pet.class)
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet;
 
-    public User() {}
+    public User() {
+    }
 
-    public User(Long regionId, Long petId) {
-        this.regionId = regionId;
-        this.petId = petId;
+    public User(Region region, Pet pet) {
+        this.region = region;
+        this.pet = pet;
     }
 }
