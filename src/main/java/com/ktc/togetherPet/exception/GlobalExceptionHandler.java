3e0a2ff5f1ee99f84db222eb3f033cde24a6c884
pinkbeenyeti <rbgusdn12@gmail.com>
2024-09-25 18:04:01 +0900

    feat: 전역에서 발생하는 예외 처리를 담당하는 GlobalExceptionHandler 생성

---
+++ src/main/java/com/ktc/togetherPet/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.ktc.togetherPet.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, String>> handleCustomException(CustomException customException) {
+        Map<String, String> response = new HashMap<>();
+        String message = customException.getMessage();
+        response.put("message", customException.getMessage());
+
+        return new ResponseEntity<>(response, customException.getStatus());
+    }
+}
