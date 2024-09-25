fac58927ff81885e52b61fac9960eba22c13ee2b
pinkbeenyeti <rbgusdn12@gmail.com>
2024-09-25 18:00:41 +0900

    feat: user 엔티티 관련 비즈니스 로직을 처리하는 UserService 생성

---
+++ src/main/java/com/ktc/togetherPet/service/UserService.java
@@ -0,0 +1,28 @@
+package com.ktc.togetherPet.service;
+
+import com.ktc.togetherPet.model.dto.user.UserDTO;
+import com.ktc.togetherPet.model.entity.User;
+import com.ktc.togetherPet.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDTO findUser(String email) {
+        User user = userRepository.findByEmail(email).orElse(null);
+
+        return new UserDTO(user);
+    }
+
+    @Transactional
+    public void createUser(String email) {
+        User user = new User(email);
+        userRepository.save(user);
+    }
+}
