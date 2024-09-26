b9223ff4b4ff41f8620e1bbedee800df96ba2c20
pinkbeenyeti <rbgusdn12@gmail.com>
2024-09-26 12:06:46 +0900

    feat: 팀원과 코드 스타일 통일을 위한 포맷팅

--- src/main/java/com/ktc/togetherPet/controller/OauthController.java
+++ src/main/java/com/ktc/togetherPet/controller/OauthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/login")
 public class OauthController {
+
     private final UserService userService;
 
     public OauthController(UserService userService) {
@@ -21,7 +22,8 @@ public class OauthController {
     }
 
     @GetMapping("/{provider}")
-    public ResponseEntity<?> handleOauth(@PathVariable String provider, @OauthUser Object oauthDTO) {
+    public ResponseEntity<?> handleOauth(@PathVariable String provider,
+        @OauthUser Object oauthDTO) {
         if (oauthDTO instanceof OauthUserDTO) {
             return ResponseEntity.ok().build();
         }
--- src/main/java/com/ktc/togetherPet/model/entity/User.java
+++ src/main/java/com/ktc/togetherPet/model/entity/User.java
@@ -1,6 +1,12 @@
 package com.ktc.togetherPet.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "user")
