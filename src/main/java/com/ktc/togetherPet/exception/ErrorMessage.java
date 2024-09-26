b45ad83ab1645e7a06a6280cb9fd40a2fa41e7cd
pinkbeenyeti <rbgusdn12@gmail.com>
2024-09-26 10:52:32 +0900

    feat: 예외에 대한 에러메시지를 담아놓은 클래스 생성

---
+++ src/main/java/com/ktc/togetherPet/exception/ErrorMessage.java
@@ -0,0 +1,7 @@
+package com.ktc.togetherPet.exception;
+
+public class ErrorMessage {
+    public static final String apiInvalid = "유효하지 않은 카카오 api입니다.";
+    public static final String headerInvalid = "유효하지 않은 헤더입니다.";
+    public static final String providerInvalid = "유효하지 않은 Oauth입니다.";
+}
