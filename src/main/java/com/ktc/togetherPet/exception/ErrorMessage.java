38a61e413d96404443419e509c9b64d4e3f0e76d
pinkbeenyeti <rbgusdn12@gmail.com>
2024-09-27 22:47:16 +0900

    feat: 토큰 인증 시 발생하는 예외를 던지는 메서드, 메시지 추가

--- src/main/java/com/ktc/togetherPet/exception/ErrorMessage.java
+++ src/main/java/com/ktc/togetherPet/exception/ErrorMessage.java
@@ -4,6 +4,7 @@ public class ErrorMessage {
 
     public static final String apiInvalid = "유효하지 않은 카카오 api입니다.";
     public static final String headerInvalid = "유효하지 않은 헤더입니다.";
+    public static final String tokenInvalid = "유효하지 않은 토큰입니다.";
     public static final String providerInvalid = "유효하지 않은 Oauth입니다.";
     public static final String EXPIRED_TOKEN = "만료된 토큰입니다.";
     public static final String INVALID_USER = "유효하지 않는 사용자입니다.";
