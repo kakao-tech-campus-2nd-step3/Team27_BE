1b74d0109570ee6dc8c10e916f5b7b97a373ffb3
yoong-saks <wjdghtjd06@gmail.com>
2024-09-27 18:33:13 +0900

    feat: Report Repo 생성

---
+++ src/main/java/com/ktc/togetherPet/repository/ReportRepository.java
@@ -0,0 +1,11 @@
+package com.ktc.togetherPet.repository;
+
+import com.ktc.togetherPet.model.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+
+}
