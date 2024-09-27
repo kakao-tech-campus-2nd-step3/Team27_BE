b1ab00a2d0ebea0c0a53d136a8cb1f2b7bafb827
yoong-saks <wjdghtjd06@gmail.com>
2024-09-27 18:34:08 +0900

    feat: SuspectController 생성

---
+++ src/main/java/com/ktc/togetherPet/controller/SuspectController.java
@@ -0,0 +1,12 @@
+package com.ktc.togetherPet.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/api/missing/suspect")
+public class SuspectController {
+
+}
