541923de30da038476dc2e6beb54e965484040da
Lee Hyungi <geist9110@naver.com>
2024-09-27 17:06:37 +0900

    feat: 에러 메시지 및 예외 추가
    만료된 토큰, 유효하지 않은 사용자, 애완동물의 개월수, 위치, 종에 대한 에러 메시지 및 예외 추가
    
    Related to: #17

--- src/main/java/com/ktc/togetherPet/exception/CustomException.java
+++ src/main/java/com/ktc/togetherPet/exception/CustomException.java
@@ -1,8 +1,19 @@
 package com.ktc.togetherPet.exception;
 
+import static com.ktc.togetherPet.exception.ErrorMessage.BREED_NOT_FOUND;
+import static com.ktc.togetherPet.exception.ErrorMessage.EXPIRED_TOKEN;
+import static com.ktc.togetherPet.exception.ErrorMessage.INVALID_DATE;
+import static com.ktc.togetherPet.exception.ErrorMessage.INVALID_LOCATION;
+import static com.ktc.togetherPet.exception.ErrorMessage.INVALID_PET_MONTH;
+import static com.ktc.togetherPet.exception.ErrorMessage.INVALID_USER;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 import org.springframework.http.HttpStatus;
 
 public class CustomException extends RuntimeException {
+
     private final HttpStatus status;
 
     public CustomException(String message, HttpStatus status) {
@@ -24,5 +35,29 @@ public class CustomException extends RuntimeException {
 
     public static CustomException invalidProviderException() {
         return new CustomException(ErrorMessage.providerInvalid, HttpStatus.BAD_REQUEST);
+    }
+
+    public static CustomException invalidUserException() {
+        return new CustomException(INVALID_USER, UNAUTHORIZED);
+    }
+
+    public static CustomException expiredTokenException() {
+        return new CustomException(EXPIRED_TOKEN, UNAUTHORIZED);
+    }
+
+    public static CustomException breedNotFoundException() {
+        return new CustomException(BREED_NOT_FOUND, NOT_FOUND);
+    }
+
+    public static CustomException invalidPetBirthMonthException() {
+        return new CustomException(INVALID_PET_MONTH, BAD_REQUEST);
+    }
+
+    public static CustomException invalidLocaltionException() {
+        return new CustomException(INVALID_LOCATION, BAD_REQUEST);
+    }
+
+    public static CustomException invalidDateException() {
+        return new CustomException(INVALID_DATE, BAD_REQUEST);
     }
 }
