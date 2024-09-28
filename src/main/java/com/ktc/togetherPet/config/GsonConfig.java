fd364db00f228bd60ac97c170107f3fc628fccb1
Lee Hyungi <geist9110@naver.com>
2024-09-28 17:17:14 +0900

    feat: Gson 설정 추가
    Gson은 LocalDateTime을 Json으로 변환시키기 위해서는 별도의 설정이 필요함

---
+++ src/main/java/com/ktc/togetherPet/config/GsonConfig.java
@@ -0,0 +1,62 @@
+package com.ktc.togetherPet.config;
+
+import static com.google.gson.FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GsonConfig {
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+            .setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES)
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
+            .setPrettyPrinting()
+            .create();
+    }
+
+    private static class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
+
+        private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+            "yyyy-MM-dd HH:mm:ss");
+
+        @Override
+        public JsonElement serialize(
+            LocalDateTime localDateTime,
+            Type typeOfSrc,
+            JsonSerializationContext context
+        ) {
+            return new JsonPrimitive(formatter.format(localDateTime));
+        }
+    }
+
+    private static class LocalDateTimeDeserializer implements JsonDeserializer<LocalDateTime> {
+
+        @Override
+        public LocalDateTime deserialize(
+            JsonElement json,
+            Type typeOfT,
+            JsonDeserializationContext context
+        ) throws JsonParseException {
+            return LocalDateTime.parse(
+                json.getAsString(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            );
+        }
+    }
+}
