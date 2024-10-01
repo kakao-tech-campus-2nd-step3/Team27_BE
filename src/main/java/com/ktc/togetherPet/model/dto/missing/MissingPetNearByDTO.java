fe3c5fc519a24fe5f86db4122632067a7700f6d6
Lee Hyungi <geist9110@naver.com>
2024-10-01 18:23:22 +0900

    feat: 근처 실종동물 찾기 API를 위한 DTO 추가

---
+++ src/main/java/com/ktc/togetherPet/model/dto/missing/MissingPetNearByDTO.java
@@ -0,0 +1,14 @@
+package com.ktc.togetherPet.model.dto.missing;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record MissingPetNearByDTO(
+    long petId,
+    float latitude,
+    float longitude,
+    String petImageUrl
+) {
+
+}
