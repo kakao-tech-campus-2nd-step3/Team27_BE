23e31f804b9f64d880f574a97044320512d9963c
Lee Hyungi <geist9110@naver.com>
2024-10-01 18:23:45 +0900

    feat: 근처 실종동물 찾기 API를 위한 컨트롤러 추가

--- src/main/java/com/ktc/togetherPet/controller/MissingController.java
+++ src/main/java/com/ktc/togetherPet/controller/MissingController.java
@@ -1,15 +1,20 @@
 package com.ktc.togetherPet.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 import com.ktc.togetherPet.annotation.OauthUser;
 import com.ktc.togetherPet.model.dto.missing.MissingPetDTO;
+import com.ktc.togetherPet.model.dto.missing.MissingPetNearByDTO;
 import com.ktc.togetherPet.model.dto.oauth.OauthUserDTO;
 import com.ktc.togetherPet.service.MissingService;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +35,15 @@ public class MissingController {
 
         missingService.registerMissingPet(oauthUserDTO, missingPetDTO);
         return ResponseEntity.status(CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MissingPetNearByDTO>> getMissingPetsNearByRegion(
+        @RequestParam("latitude") float latitude,
+        @RequestParam("longitude") float longitude
+    ) {
+        return ResponseEntity
+            .status(OK)
+            .body(missingService.getMissingPetsNearBy(latitude, longitude));
     }
 }
