5e03b266c4b0940b1fd8ce974c2b67b52fe7eace
pinkbeenyeti <rbgusdn12@gmail.com>
2024-10-03 00:31:15 +0900

    feat: pet 엔티티와 관련된 비즈니스 로직을 처리하는 서비스 생성
    body: petId로 pet 엔티티를 찾는 findPetById 메서드, 새로운 pet 엔티티를 생성하는 createPet 메서드, pet 엔티티에 pet 이미지를 저장하는 경로를 설정하는 setImageSrc 메서드 추가

---
+++ src/main/java/com/ktc/togetherPet/service/PetService.java
@@ -0,0 +1,41 @@
+package com.ktc.togetherPet.service;
+
+import com.ktc.togetherPet.exception.CustomException;
+import com.ktc.togetherPet.model.dto.pet.PetRegisterDTO;
+import com.ktc.togetherPet.model.entity.Breed;
+import com.ktc.togetherPet.model.entity.Pet;
+import com.ktc.togetherPet.model.vo.BirthMonth;
+import com.ktc.togetherPet.repository.PetRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetService {
+
+    private final PetRepository petRepository;
+
+    public PetService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    public Pet findPetById(Long petId) {
+        return petRepository.findById(petId).orElseThrow(CustomException::petNotFoundException);
+    }
+
+    public Long createPet(PetRegisterDTO petRegisterDTO) {
+        BirthMonth birthMonth = new BirthMonth(petRegisterDTO.petBirthMonth());
+        Breed breed = new Breed(petRegisterDTO.petType());
+
+        Pet pet = new Pet(petRegisterDTO.petName(), birthMonth,
+            breed, petRegisterDTO.isNeutering());
+        Pet savedPet = petRepository.save(pet);
+
+        return savedPet.getId();
+    }
+
+    public void setImageSrc(Long petId, String imageSrc) {
+        Pet pet = petRepository.findById(petId).orElseThrow(CustomException::petNotFoundException);
+        pet.setImageSrc(imageSrc);
+
+        petRepository.save(pet);
+    }
+}
