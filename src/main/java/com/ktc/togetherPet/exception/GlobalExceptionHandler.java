7be24f735185be54a7f9c657f961cfdab6140e66
Lee Hyungi <geist9110@naver.com>
2024-10-03 13:10:52 +0900

    refactor: Map을 사용한 ErrorHandling에서 ErrorResponse 객체로 변환

--- src/main/java/com/ktc/togetherPet/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ktc/togetherPet/exception/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.ktc.togetherPet.exception;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,18 +10,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<Map<String, String>> handleCustomException(
-        CustomException customException) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", customException.getMessage());
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException customException) {
+        ErrorResponse response = new ErrorResponse(customException.getMessage());
 
         return new ResponseEntity<>(response, customException.getStatus());
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<Map<String, String>> handleIOException(IOException ioException) {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "파일 처리 중 오류가 발생했습니다: " + ioException.getMessage());
+    public ResponseEntity<ErrorResponse> handleIOException(IOException ioException) {
+        ErrorResponse response = new ErrorResponse(
+            "파일 처리 중 오류가 발생했습니다: " + ioException.getMessage()
+        );
 
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
