8922445b71d11c7a645915af1e400b46b15f2a15
yoong-saks <wjdghtjd06@gmail.com>
2024-10-04 22:23:54 +0900

    feat: ReportService / createReport 기능 추가

--- src/main/java/com/ktc/togetherPet/service/ReportService.java
+++ src/main/java/com/ktc/togetherPet/service/ReportService.java
@@ -1,5 +1,13 @@
 package com.ktc.togetherPet.service;
 
+import com.ktc.togetherPet.exception.CustomException;
+import com.ktc.togetherPet.model.dto.suspect.SuspectRequestDTO;
+import com.ktc.togetherPet.model.entity.Breed;
+import com.ktc.togetherPet.model.entity.Missing;
+import com.ktc.togetherPet.model.entity.Report;
+import com.ktc.togetherPet.model.entity.User;
+import com.ktc.togetherPet.model.vo.Location;
+import com.ktc.togetherPet.repository.MissingRepository;
 import com.ktc.togetherPet.repository.ReportRepository;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +15,28 @@ import org.springframework.stereotype.Service;
 public class ReportService {
 
     private final ReportRepository reportRepository;
+    private final MissingRepository missingRepository;
 
-    public ReportService(ReportRepository reportRepository) {
+    public ReportService(ReportRepository reportRepository, MissingRepository missingRepository) {
         this.reportRepository = reportRepository;
+        this.missingRepository = missingRepository;
+    }
+
+    public Long createReport(User user, SuspectRequestDTO suspectRequestDTO) {
+
+        Location location = new Location(
+            suspectRequestDTO.foundLatitude(),
+            suspectRequestDTO.foundLongitude()
+        );
+
+        Report report = reportRepository.save(
+            new Report(
+                user,
+                suspectRequestDTO.foundDate(),
+                location
+            )
+        );
+
+        return report.getId();
     }
 }
