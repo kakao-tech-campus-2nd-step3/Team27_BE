a0f637aaa2905468b98a0a809dc49b812f247355
yoong-saks <wjdghtjd06@gmail.com>
2024-10-06 15:21:39 +0900

    feat: ReportService / 실종의심동물리스트 받기 기능 구현

--- src/main/java/com/ktc/togetherPet/service/ReportService.java
+++ src/main/java/com/ktc/togetherPet/service/ReportService.java
@@ -1,14 +1,18 @@
 package com.ktc.togetherPet.service;
 
 import com.ktc.togetherPet.exception.CustomException;
+import com.ktc.togetherPet.model.dto.suspect.ReportNearByDTO;
 import com.ktc.togetherPet.model.dto.suspect.SuspectRequestDTO;
 import com.ktc.togetherPet.model.entity.Breed;
+import com.ktc.togetherPet.model.entity.ImageRelation.ImageEntityType;
 import com.ktc.togetherPet.model.entity.Missing;
 import com.ktc.togetherPet.model.entity.Report;
 import com.ktc.togetherPet.model.entity.User;
 import com.ktc.togetherPet.model.vo.Location;
 import com.ktc.togetherPet.repository.MissingRepository;
 import com.ktc.togetherPet.repository.ReportRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,11 +21,14 @@ public class ReportService {
     private final ReportRepository reportRepository;
     private final MissingRepository missingRepository;
     private final KakaoMapService kakaoMapService;
+    private final ImageService imageService;
 
-    public ReportService(ReportRepository reportRepository, MissingRepository missingRepository, KakaoMapService kakaoMapService) {
+    public ReportService(ReportRepository reportRepository, MissingRepository missingRepository, KakaoMapService kakaoMapService,
+        ImageService imageService) {
         this.reportRepository = reportRepository;
         this.missingRepository = missingRepository;
         this.kakaoMapService = kakaoMapService;
+        this.imageService = imageService;
     }
 
     public Long createReport(User user, SuspectRequestDTO suspectRequestDTO) {
@@ -41,6 +48,29 @@ public class ReportService {
         );
 
         return report.getId();
+    }
+
+    public List<ReportNearByDTO> getReportsByLocation(float latitude, float longitude) {
+        Location location = new Location(latitude, longitude);
+        long regionCode = kakaoMapService.getRegionCodeFromKakao(location);
+
+        /** XXX: 이 부분 missing이 null인 경우에 받을 지 아니면 전체 리포트를 받을 지 상의 필요
+            * missing이 null인 것만 받아옴 = 실종신고자에게 제보한 것은 제외함 = findAllByRegionCodeAndMissingNotNull
+            * 전체 리포트를 받아옴 = 실종신고자에게 제보한 것도 포함함 = findAllByRegionCode
+         **/
+        return reportRepository.findAllByRegionCode(regionCode)
+            .stream()
+            .map(report -> {
+                    String representImagePath = imageService.getRepresentativeImageById(ImageEntityType.REPORT, report.getId());
+                    return new ReportNearByDTO(
+                        report.getId(),
+                        report.getLocation().getLatitude(),
+                        report.getLocation().getLongitude(),
+                        representImagePath
+                    );
+            })
+            .collect(Collectors.toList());
+
     }
 
     public void setBreed(Long reportId, String breed) {
