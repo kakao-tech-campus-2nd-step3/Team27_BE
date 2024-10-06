1a49b5772084206c0918244dc32b130483af1a2b
yoong-saks <wjdghtjd06@gmail.com>
2024-10-06 14:14:33 +0900

    fix: imageRelationRepo / List를 반환하도록 변경

--- src/main/java/com/ktc/togetherPet/repository/ImageRelationRepository.java
+++ src/main/java/com/ktc/togetherPet/repository/ImageRelationRepository.java
@@ -2,12 +2,12 @@ package com.ktc.togetherPet.repository;
 
 import com.ktc.togetherPet.model.entity.ImageRelation.ImageEntityType;
 import com.ktc.togetherPet.model.entity.ImageRelation.ImageRelation;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ImageRelationRepository extends JpaRepository<ImageRelation, Long> {
 
-    Optional<ImageRelation> findByImageEntityTypeAndEntityId(ImageEntityType imageEntityType, Long entityId);
+    List<ImageRelation> findByImageEntityTypeAndEntityId(ImageEntityType imageEntityType, Long entityId);
 }
