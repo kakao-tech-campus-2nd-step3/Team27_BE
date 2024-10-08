c38c48a2d2449a3930e7f752181968289e6ee73b
pinkbeenyeti <rbgusdn12@gmail.com>
2024-10-09 01:52:10 +0900

    fix: jwt 토큰과 status 코드를 함께 반환하도록 수정

--- src/main/java/com/ktc/togetherPet/service/OauthService.java
+++ src/main/java/com/ktc/togetherPet/service/OauthService.java
@@ -1,7 +1,8 @@
 package com.ktc.togetherPet.service;
 
-import com.ktc.togetherPet.exception.CustomException;
 import com.ktc.togetherPet.jwtUtil.JwtUtil;
+import com.ktc.togetherPet.model.dto.oauth.OauthSuccessDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,33 +10,24 @@ public class OauthService {
 
     private final JwtUtil jwtUtil;
     private final UserService userService;
-    private final KakaoOauthService kakaoOauthService;
 
-    public OauthService(JwtUtil jwtUtil, UserService userService,
-        KakaoOauthService kakaoOauthService) {
+    public OauthService(JwtUtil jwtUtil, UserService userService) {
         this.jwtUtil = jwtUtil;
         this.userService = userService;
-        this.kakaoOauthService = kakaoOauthService;
     }
 
-    public String processOauth(String provider, String accessToken) {
-        String email = getEmailFromProvider(provider, accessToken);
-        ensureUserExists(email);
+    public OauthSuccessDTO processOauth(String email) {
+        HttpStatus status = ensureUserExists(email);
 
-        return jwtUtil.makeToken(email);
+        return new OauthSuccessDTO(status, jwtUtil.makeToken(email));
     }
 
-    private void ensureUserExists(String email) {
+    private HttpStatus ensureUserExists(String email) {
         if (!userService.userExists(email)) {
             userService.createUser(email);
-        }
-    }
-
-    private String getEmailFromProvider(String provider, String accessToken) {
-        if (provider.equals("kakao")) {
-            return kakaoOauthService.getEmailFromToken(accessToken);
+            return HttpStatus.CREATED;
         }
 
-        throw CustomException.invalidProviderException();
+        return HttpStatus.OK;
     }
 }
