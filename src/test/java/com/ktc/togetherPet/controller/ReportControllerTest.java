84c3a24e5e4932ed55cafcec24bc116ae952f252
Lee Hyungi <geist9110@naver.com>
2024-10-09 18:18:48 +0900

    test: 실종 등록된 애완동물에 대한 제보 테스트 추가

--- src/test/java/com/ktc/togetherPet/controller/ReportControllerTest.java
+++ src/test/java/com/ktc/togetherPet/controller/ReportControllerTest.java
@@ -37,7 +37,7 @@ class ReportControllerTest extends RestDocsTestSupport {
     private OauthUserArgumentResolver oauthUserArgumentResolver;
 
     @Test
-    @DisplayName("제보 등록 테스트/createReport")
+    @DisplayName("제보 등록 테스트(임의 제보)/createReport")
     void 제보_등록_임의제보() throws Exception {
         // given
         ReportCreateRequestDTO reportCreateRequestDTO = new ReportCreateRequestDTO(
@@ -105,6 +105,84 @@ class ReportControllerTest extends RestDocsTestSupport {
                     fieldWithPath("description").description("제보 설명"),
                     fieldWithPath("breed").description("제보하고자 하는 동물의 종"),
                     fieldWithPath("gender").description("제보하고자 하는 동물의 성별")
+                )
+            ));
+
+        verify(reportService, times(1))
+            .createReport(reportCreateRequestDTO, List.of(image1, image2), oauthUserDTO);
+
+    }
+
+    @Test
+    @DisplayName("제보 등록 테스트(실종 등록된 제보)/createReport")
+    void 제보_등록_실종등록() throws Exception {
+        // given
+        ReportCreateRequestDTO reportCreateRequestDTO = new ReportCreateRequestDTO(
+            "testColor",
+            15.0D,
+            15.0D,
+            LocalDateTime.of(2024, 10, 9, 17, 53, 11),
+            "testDescription",
+            "testBreed",
+            "testGender",
+            1L
+        );
+
+        MockMultipartFile reportCreateRequestDTOMock = new MockMultipartFile(
+            "reportCreateRequestDTO",
+            "reportCreateRequestDTO",
+            APPLICATION_JSON_VALUE,
+            toJson(reportCreateRequestDTO).getBytes()
+        );
+
+        MockMultipartFile image1 = new MockMultipartFile(
+            "files",
+            "image1.jpeg",
+            IMAGE_JPEG_VALUE,
+            "image1".getBytes()
+        );
+        MockMultipartFile image2 = new MockMultipartFile(
+            "files",
+            "image2.jpeg",
+            IMAGE_JPEG_VALUE,
+            "image2".getBytes()
+
+        );
+
+        String token = "Bearer testToken";
+        OauthUserDTO oauthUserDTO = new OauthUserDTO("test@email.com");
+
+        // when
+        when(oauthUserArgumentResolver.supportsParameter(any()))
+            .thenReturn(true);
+        when(oauthUserArgumentResolver.resolveArgument(any(), any(), any(), any()))
+            .thenReturn(oauthUserDTO);
+
+        ResultActions result = mockMvc.perform(
+            multipart("/api/v0/report")
+                .file(reportCreateRequestDTOMock)
+                .file(image1)
+                .file(image2)
+                .header("Authorization", token)
+        );
+
+        // then
+        result.andExpect(status().isCreated())
+            .andDo(restDocs.document(
+                requestParts(
+                    partWithName("reportCreateRequestDTO").description("제보를 위한 정보"),
+                    partWithName("files").description("제보하고자 하는 동물의 이미지")
+                ),
+                requestPartFields(
+                    "reportCreateRequestDTO",
+                    fieldWithPath("color").description("제보하고자 하는 동물의 색상"),
+                    fieldWithPath("found_latitude").description("제보하고자 하는 위치의 위도"),
+                    fieldWithPath("found_longitude").description("제보하고자 하는 위치의 경도"),
+                    fieldWithPath("found_date").description("제보하고자 하는 시각"),
+                    fieldWithPath("description").description("제보 설명"),
+                    fieldWithPath("breed").description("제보하고자 하는 동물의 종"),
+                    fieldWithPath("gender").description("제보하고자 하는 동물의 성별"),
+                    fieldWithPath("missing_id").description("제보하고자 하는 실종에 대한 id")
                 )
             ));
 
