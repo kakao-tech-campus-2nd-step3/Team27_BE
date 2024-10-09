41b0e150d0a49dea05fc2f66941bf71a7a1288c7
Lee Hyungi <geist9110@naver.com>
2024-10-09 18:28:58 +0900

    test: 받은 제보에 대한 API 테스트 추가 및 ReportResponseDTO의 JsonNaming 추가

--- src/main/java/com/ktc/togetherPet/model/dto/report/ReportResponseDTO.java
+++ src/main/java/com/ktc/togetherPet/model/dto/report/ReportResponseDTO.java
@@ -1,5 +1,9 @@
 package com.ktc.togetherPet.model.dto.report;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(SnakeCaseStrategy.class)
 public record ReportResponseDTO(
     long id,
     double latitude,
--- src/test/java/com/ktc/togetherPet/controller/ReportControllerTest.java
+++ src/test/java/com/ktc/togetherPet/controller/ReportControllerTest.java
@@ -4,18 +4,25 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestPartFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.ktc.togetherPet.annotation.OauthUserArgumentResolver;
 import com.ktc.togetherPet.model.dto.oauth.OauthUserDTO;
 import com.ktc.togetherPet.model.dto.report.ReportCreateRequestDTO;
+import com.ktc.togetherPet.model.dto.report.ReportResponseDTO;
 import com.ktc.togetherPet.service.ReportService;
 import com.ktc.togetherPet.testConfig.RestDocsTestSupport;
 import java.time.LocalDateTime;
@@ -189,5 +196,63 @@ class ReportControllerTest extends RestDocsTestSupport {
         verify(reportService, times(1))
             .createReport(reportCreateRequestDTO, List.of(image1, image2), oauthUserDTO);
 
+    }
+
+    @Test
+    @DisplayName("받은 제보 확인 테스트/getReceivedReports")
+    void 받은_제보_확인() throws Exception {
+        // given
+        String token = "Bearer testToken";
+        OauthUserDTO oauthUserDTO = new OauthUserDTO("test@email.com");
+        List<ReportResponseDTO> actual = List.of(
+            new ReportResponseDTO(
+                1L,
+                15.0D,
+                15.0D,
+                "https://together-pet/api/v0/images/test-image-1"
+            ),
+            new ReportResponseDTO(
+                2L,
+                30.0D,
+                25.0D,
+                "https://together-pet/api/v0/images/test-image-2"
+            )
+        );
+
+        // when
+        when(oauthUserArgumentResolver.supportsParameter(any()))
+            .thenReturn(true);
+
+        when(oauthUserArgumentResolver.resolveArgument(any(), any(), any(), any()))
+            .thenReturn(oauthUserDTO);
+
+        when(reportService.getReceivedReports(oauthUserDTO))
+            .thenReturn(actual);
+
+        ResultActions result = mockMvc.perform(
+            get("/api/v0/report/user")
+                .contentType(APPLICATION_JSON_VALUE)
+                .header("Authorization", token)
+        );
+
+        // then
+        result.andExpectAll(
+            status().isOk(),
+            content().contentType(APPLICATION_JSON),
+            content().json(toJson(actual))
+        ).andDo(restDocs.document(
+            requestHeaders(
+                headerWithName("Authorization").description("인증을 위한 Bearer 토큰")
+            ),
+            responseFields(
+                fieldWithPath("[].id").description("제보 id"),
+                fieldWithPath("[].latitude").description("제보 위도"),
+                fieldWithPath("[].longitude").description("제보 경도"),
+                fieldWithPath("[].image_url").description("대표 이미지 경로")
+            )
+        ));
+
+        verify(reportService, times(1))
+            .getReceivedReports(oauthUserDTO);
     }
 }
