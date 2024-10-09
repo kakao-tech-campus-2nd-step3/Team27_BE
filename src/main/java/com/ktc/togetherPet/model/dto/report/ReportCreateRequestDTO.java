dae898fad859a4f8b69bb2f1d96633441a26ed96
Lee Hyungi <geist9110@naver.com>
2024-10-09 15:23:30 +0900

    fix: ReportCreateRequestDTO의 JsonNaming 추가

--- src/main/java/com/ktc/togetherPet/model/dto/report/ReportCreateRequestDTO.java
+++ src/main/java/com/ktc/togetherPet/model/dto/report/ReportCreateRequestDTO.java
@@ -1,9 +1,12 @@
 package com.ktc.togetherPet.model.dto.report;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+@JsonNaming(SnakeCaseStrategy.class)
 public record ReportCreateRequestDTO(
     @NotNull
     String color,
