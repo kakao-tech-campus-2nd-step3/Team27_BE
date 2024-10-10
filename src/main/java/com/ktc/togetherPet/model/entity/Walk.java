2c7acd4accb75f2d2af499cf878cac6d174f714a
yoong-saks <wjdghtjd06@gmail.com>
2024-10-10 21:16:09 +0900

    feat: Walk Entity 생성

---
+++ src/main/java/com/ktc/togetherPet/model/entity/Walk.java
@@ -0,0 +1,37 @@
+package com.ktc.togetherPet.model.entity;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "walk")
+@NoArgsConstructor(access = PROTECTED)
+public class Walk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = Pet.class)
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet;
+
+    @Column(nullable = false)
+    private Float distance;
+
+    @Column(nullable = false)
+    private LocalDateTime walkDate;
+
+    @Column(nullable = false)
+    private Long walkTime;
+}
