61d301b6d94c657603a97681bddacc95034625a4
yoong-saks <wjdghtjd06@gmail.com>
2024-10-10 21:09:17 +0900

    feat: WalkController 생성

---
+++ src/main/java/com/ktc/togetherPet/controller/WalkController.java
@@ -0,0 +1,14 @@
+package com.ktc.togetherPet.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v0/walk")
+@RequiredArgsConstructor
+public class WalkController {
+
+}
