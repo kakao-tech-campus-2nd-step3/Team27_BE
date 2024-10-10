2472e86ebace5b064c6e22c6af1f5f98199f7754
yoong-saks <wjdghtjd06@gmail.com>
2024-10-10 21:10:27 +0900

    feat: WalkService 생성

---
+++ src/main/java/com/ktc/togetherPet/service/WalkService.java
@@ -0,0 +1,11 @@
+package com.ktc.togetherPet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WalkService {
+
+
+}
