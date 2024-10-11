a4e5ab9960594e6726209efb9fba1f69094abc90
yoong-saks <wjdghtjd06@gmail.com>
2024-10-11 22:32:37 +0900

    feat : WalkController / Post Get 구현

--- src/main/java/com/ktc/togetherPet/controller/WalkController.java
+++ src/main/java/com/ktc/togetherPet/controller/WalkController.java
@@ -1,8 +1,17 @@
 package com.ktc.togetherPet.controller;
 
+import com.ktc.togetherPet.annotation.OauthUser;
+import com.ktc.togetherPet.apiResponse.CustomResponse;
+import com.ktc.togetherPet.model.dto.oauth.OauthUserDTO;
+import com.ktc.togetherPet.model.dto.walk.CalorieResponseDTO;
+import com.ktc.togetherPet.model.dto.walk.WalkRequestDTO;
+import com.ktc.togetherPet.model.dto.walk.WalkResponseDTO;
+import com.ktc.togetherPet.service.WalkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WalkController {
 
+    private final WalkService walkService;
+
+    @PostMapping
+    public ResponseEntity<CalorieResponseDTO> createWalk(
+        @OauthUser OauthUserDTO oauthUserDTO,
+        @RequestBody WalkRequestDTO walkRequestDTO
+    ) {
+        return CustomResponse.ok(walkService.createWalk(oauthUserDTO, walkRequestDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<WalkResponseDTO> getWalkInformation(
+        @OauthUser OauthUserDTO oauthUserDTO
+    ) {
+        return CustomResponse.ok(
+            walkService.getWalkInformation(oauthUserDTO)
+        );
+    }
 }
