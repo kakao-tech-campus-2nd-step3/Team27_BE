ea9fcfa5044ab097719433fa333e8ceb09f89ace
Lee Hyungi <geist9110@naver.com>
2024-10-17 19:53:31 +0900

    test: 지역 코드를 기반으로 임의의 실종 정보를 가져오는 테스트 코드 추가

--- src/test/java/com/ktc/togetherPet/repository/ReportRepositoryTest.java
+++ src/test/java/com/ktc/togetherPet/repository/ReportRepositoryTest.java
@@ -10,6 +10,7 @@ import com.ktc.togetherPet.model.entity.User;
 import com.ktc.togetherPet.model.vo.Location;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +32,15 @@ class ReportRepositoryTest {
 
     @Autowired
     private PetRepository petRepository;
+
     @Autowired
     private MissingRepository missingRepository;
 
-    @Test
-    @DisplayName("실종을 바탕으로 모든 제보 찾기 테스트/findAllByMissing")
-    void 실종을_바탕으로_모든_제보_찾기() {
-        // given
+    private Missing givenMissing;
+    private List<Report> givenReport;
+
+    @BeforeEach
+    void setUp() {
         Breed givenBreed = new Breed("testPetBreed");
 
         Pet givenPet = new Pet(
@@ -47,7 +50,13 @@ class ReportRepositoryTest {
             true
         );
 
-        Missing givenMissing = new Missing(
+        List<User> givenUser = List.of(
+            new User("test1@email.com"),
+            new User("test2@email.com"),
+            new User("test3@email.com")
+        );
+
+        givenMissing = new Missing(
             givenPet,
             true,
             LocalDateTime.of(2024, 10, 17, 19, 39, 11),
@@ -56,13 +65,7 @@ class ReportRepositoryTest {
             "testMissingDescription"
         );
 
-        List<User> givenUser = List.of(
-            new User("test1@email.com"),
-            new User("test2@email.com"),
-            new User("test3@email.com")
-        );
-
-        List<Report> givenReport = List.of(
+        givenReport = List.of(
             new Report(
                 givenUser.get(0),
                 LocalDateTime.of(2024, 10, 17, 19, 19, 11),
@@ -103,22 +106,47 @@ class ReportRepositoryTest {
         givenReport.get(0).setMissing(givenMissing);
         givenReport.get(4).setMissing(givenMissing);
 
+        breedRepository.save(givenBreed);
+        petRepository.save(givenPet);
+        userRepository.saveAll(givenUser);
+        missingRepository.save(givenMissing);
+    }
+
+    @Test
+    @DisplayName("실종을 바탕으로 모든 제보 찾기 테스트/findAllByMissing")
+    void 실종을_바탕으로_모든_제보_찾기() {
+        // given
         List<Report> expect = List.of(
             givenReport.get(0),
             givenReport.get(4)
         );
 
         // when
-        breedRepository.save(givenBreed);
-        petRepository.save(givenPet);
-        userRepository.saveAll(givenUser);
-        missingRepository.save(givenMissing);
         reportRepository.saveAll(givenReport);
 
         // then
         List<Report> actual = reportRepository.findAllByMissing(givenMissing);
 
-        assertEquals(actual, expect);
+        assertEquals(expect, actual);
 
+    }
+
+    @Test
+    @DisplayName("지역 코드를 기반으로 실종 정보가 없는 임의의 제보를 가져오는 테스트/findAllByRegionCodeAndMissingNull")
+    void 지역_코드를_기반으로_실종_정보가_없는_임의의_제보를_가져오기() {
+        // given
+        long regionCode = 1L;
+        List<Report> expect = List.of(
+            givenReport.get(1),
+            givenReport.get(2)
+        );
+
+        // when
+        reportRepository.saveAll(givenReport);
+
+        // then
+        List<Report> actual = reportRepository.findAllByRegionCodeAndMissingNull(regionCode);
+
+        assertEquals(expect, actual);
     }
 }
