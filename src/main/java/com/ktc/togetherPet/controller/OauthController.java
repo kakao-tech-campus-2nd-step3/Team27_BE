19d401675ada7ad4fb4a909186a0e166fe262154
pinkbeenyeti <rbgusdn12@gmail.com>
2024-10-18 21:55:37 +0900

    fix: rest 컨트롤러를 사용하도록 수정, 헤더가 아닌 바디로 이메일을 받기 때문에 post를 사용하도록 수정

--- src/main/java/com/ktc/togetherPet/controller/OauthController.java
+++ src/main/java/com/ktc/togetherPet/controller/OauthController.java
@@ -7,19 +7,19 @@ import com.ktc.togetherPet.service.OauthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/login")
 @RequiredArgsConstructor
 public class OauthController {
 
     private final OauthService oauthService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<?> handleOauth(
         @Valid @RequestBody OauthRequestDTO oauthRequestDTO
     ) {
