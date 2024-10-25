e11eac8e31b693bb7dbb62594d4cd0a30de706e0
yoong-saks <wjdghtjd06@gmail.com>
2024-10-25 16:45:19 +0900

    feat: CalulateFlag 로직 추가 조금 더 고민해볼 필요가 있을 것 같아요

--- src/main/java/com/ktc/togetherPet/util/WalkCalculator.java
+++ src/main/java/com/ktc/togetherPet/util/WalkCalculator.java
@@ -1,10 +1,29 @@
 package com.ktc.togetherPet.util;
 
+import com.ktc.togetherPet.model.dto.walk.WalkInformationDTO;
+
 public class WalkCalculator {
 
     private static final double AVERAGE_CALORIE_PER_KM = 15.0;
 
     public static long calculateCalorie(float distance) {
         return (long) (distance * AVERAGE_CALORIE_PER_KM);
+    }
+
+    public static int calculateFlag(WalkInformationDTO walkInformationDTO) {
+
+        int flagValue = 0;
+
+        if(walkInformationDTO.todayWalkCount() < walkInformationDTO.averageWalkCount()) {
+            flagValue += 1;
+        }
+        if(walkInformationDTO.todayWalkTime() < walkInformationDTO.averageWalkTime()) {
+            flagValue += 1;
+        }
+        if(walkInformationDTO.todayWalkDistance() < walkInformationDTO.averageWalkDistance()) {
+            flagValue += 1;
+        }
+
+        return flagValue;
     }
 }
