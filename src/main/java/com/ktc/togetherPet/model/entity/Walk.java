9aa9d24544c9259347aa807de2f2e11649ebe389
yoong-saks <wjdghtjd06@gmail.com>
2024-11-06 10:37:25 +0900

    feat: getter 추가

--- src/main/java/com/ktc/togetherPet/model/entity/Walk.java
+++ src/main/java/com/ktc/togetherPet/model/entity/Walk.java
@@ -19,6 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 public class Walk {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
