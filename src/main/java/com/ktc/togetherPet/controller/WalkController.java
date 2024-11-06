5f24776d7d07559a79daa96112b2afe7ecb9c0e7
yoong-saks <wjdghtjd06@gmail.com>
2024-11-06 10:37:47 +0900

    feat: 산책 List 조회 API 구현

--- src/main/java/com/ktc/togetherPet/controller/WalkController.java
+++ src/main/java/com/ktc/togetherPet/controller/WalkController.java
@@ -4,15 +4,20 @@ import com.ktc.togetherPet.annotation.OauthUser;
 import com.ktc.togetherPet.apiResponse.CustomResponse;
 import com.ktc.togetherPet.model.dto.oauth.OauthUserDTO;
 import com.ktc.togetherPet.model.dto.walk.CalorieResponseDTO;
+import com.ktc.togetherPet.model.dto.walk.WalkPathByDateResponseDTO;
 import com.ktc.togetherPet.model.dto.walk.WalkRequestDTO;
 import com.ktc.togetherPet.model.dto.walk.WalkResponseDTO;
 import com.ktc.togetherPet.service.WalkService;
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,6 +44,15 @@ public class WalkController {
         );
     }
 
-    // todo : 산책 List 조회 API
-    // todo : 산책 상세 조회 API
+    @GetMapping("/paths")
+    public ResponseEntity<List<WalkPathByDateResponseDTO>> getWalkPathListByDate(
+        @OauthUser OauthUserDTO oauthUserDTO,
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        @RequestParam(required = true)
+        LocalDateTime date
+    ) {
+        return CustomResponse.ok(
+            walkService.getWalkPathByDate(oauthUserDTO, date)
+        );
+    }
 }
