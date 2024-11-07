8e9f29c92e427dc0ed50cd6a4e2a9bdfbf6183fd
pinkbeenyeti <rbgusdn12@gmail.com>
2024-11-07 11:53:36 +0900

    feat: fcm에 메시지를 보낼 때 사용하는 제목과 본문에 대한 내용들을 한 번에 관리하는 프로퍼티 클래스 생성

---
+++ src/main/java/com/ktc/togetherPet/config/property/FCMProperties.java
@@ -0,0 +1,7 @@
+package com.ktc.togetherPet.config.property;
+
+public class FCMProperties {
+    public static final String missingTile = "근처에서 실종 신고가 발견되었습니다!";
+    public static final String missingBody = "주변에 실종된 반려동물이 있습니다. 확인해보세요!";
+
+}
