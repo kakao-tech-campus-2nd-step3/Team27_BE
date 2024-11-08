4c0c9221261bd01be477d41cf4c138f794bdc2a3
yoong-saks <wjdghtjd06@gmail.com>
2024-11-08 23:08:42 +0900

    refactor: walkInformationDTO 인터페이스 기반 프로젝션으로 변경

--- src/main/java/com/ktc/togetherPet/model/dto/walk/WalkInformationDTO.java
+++ src/main/java/com/ktc/togetherPet/model/dto/walk/WalkInformationDTO.java
@@ -1,12 +1,10 @@
 package com.ktc.togetherPet.model.dto.walk;
 
-public record WalkInformationDTO (
-    Long todayWalkCount,
-    Double averageWalkCount,
-    Double todayWalkTime,
-    Double averageWalkTime,
-    Double todayWalkDistance,
-    Double averageWalkDistance
-){
-
+public interface WalkInformationDTO {
+    Long getTodayWalkCount();
+    Double getAverageWalkCount();
+    Double getTodayWalkTime();
+    Double getAverageWalkTime();
+    Double getTodayWalkDistance();
+    Double getAverageWalkDistance();
 }
